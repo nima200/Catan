25d72670052f0f261c42d1820297c3550d80b1c6
Nima <nima.adibpour@mail.mcgill.ca>
2017-01-18 20:33:34 -0500

    added new files for hex board

---
+++ Settlers of Catan/Assets/Scripts/Hex Grid/HexMesh.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class HexMesh : MonoBehaviour {
+    Mesh hexMesh;
+    List<Vector3> vertices;
+    List<int> triangles;
+
+    void awake()
+    {
+        GetComponent<MeshFilter>().mesh = hexMesh = new Mesh();
+        hexMesh.name = "Hex Mesh";
+        vertices = new List<Vector3>();
+        triangles = new List<int>();
+        
+    }
+	
+	
+}
