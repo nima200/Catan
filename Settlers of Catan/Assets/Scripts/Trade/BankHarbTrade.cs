24525b0883654d2d6a3744f54ad7c87b2a9fb64a
yko16 <chloe.schoreisz@mail.mcgill.ca>
2017-02-05 10:55:02 -0500

    Trade ui, Bank Harbour

---
+++ Settlers of Catan/Assets/Scripts/Trade/BankHarbTrade.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BankHarbTrade : MonoBehaviour
+{
+
+    
+    public GameObject bankHarbMenu;
+    public GameObject mainMenu;
+    public int ratio;         // TODO : ref to player's inventory + ratio
+                              // for now, click on TRADE button to reopen ongoing trade
+                              //TODO : make tab for ongoing trade (where?)
+
+
+    public void OpenBankHarb()
+    {
+        bankHarbMenu.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
+    public void CloseBankHarb()
+    {
+        bankHarbMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void HideBankHarb()  //TODO
+    {
+        bankHarbMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+}
+
