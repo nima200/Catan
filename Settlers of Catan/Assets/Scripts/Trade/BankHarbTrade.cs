7306546043bfb751c0b90318f44a900b6868d0e0
yko16 <chloe.schoreisz@mail.mcgill.ca>
2017-02-05 22:35:46 -0500

    Game Camera fixed + Card menu
    Camera was misplaced
    Card menu -> TODO : reformat card

--- Settlers of Catan/Assets/Scripts/Trade/BankHarbTrade.cs
+++ Settlers of Catan/Assets/Scripts/Trade/BankHarbTrade.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class BankHarbTrade : MonoBehaviour
 {
 
-    
+    public GameObject lights;
     public GameObject bankHarbMenu;
     public GameObject mainMenu;
     public int ratio;         // TODO : ref to player's inventory + ratio
@@ -18,18 +18,21 @@ public class BankHarbTrade : MonoBehaviour
     {
         bankHarbMenu.SetActive(true);
         mainMenu.SetActive(false);
+        lights.SetActive(false);
     }
 
     public void CloseBankHarb()
     {
         bankHarbMenu.SetActive(false);
         mainMenu.SetActive(true);
+        lights.SetActive(true);
     }
 
     public void HideBankHarb()  //TODO
     {
         bankHarbMenu.SetActive(false);
         mainMenu.SetActive(true);
+        lights.SetActive(true);
     }
 }
 
