c8a3f2b4e67f831029f559811eff86582de20362
Khalil Mohsin <mohsin.khalil8@gmail.com>
2017-02-27 18:19:23 -0500

    GameManager_Started
    Made a game manager singleton
    Handles all other managers
    Allows syncorization between Lobby and Game.

--- Settlers of Catan/Assets/Scripts/Game Control/GameManager.cs
+++ Settlers of Catan/Assets/Scripts/Game Control/GameManager.cs
@@ -1,21 +1,51 @@
-﻿using System.Collections;
+﻿using Prototype.NetworkLobby;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
+    // has a getter, and is a singleton.
+    private static GameManager Instance { get;  set; }
+    public LobbyManager _lobbyManager;
+    public LobbyPlayerList _lobbyplayerlist;
 
-	private static GameManager current;
-	// Use this for initialization
-	void Start () {
-		
-	}
+   //playerlist being used to make lobbyplayerlist a inumberable non synced list.
+
+    private IEnumerable<LobbyPlayer> _playerList;
+
+    // Use this for initialization
+
+    public PlayerManager _playerManager;
+
+
+    // awake for singleton. 
+    void Awake()
+    {
+        if (Instance == null) {
+            //if not, set instance to this
+            Instance = this;
+        }
+
+        //If instance already exists and it's not this:
+        else if (Instance != this) {
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+
+        //Creating new playermanager.
+        Instantiate(_playerManager);
+
+    }
+
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
-	public static GameManager getCurrent(){
-		return current;
-	}
 }
